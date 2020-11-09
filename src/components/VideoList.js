4e39f2e08a58806d664b41150eb59a15736a6621
joshembling <65712975+joshembling@users.noreply.github.com>
2020-11-09 16:13:31 +0000

    update 1.1

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import VideoItem from './VideoItem';
 
-const VideoList = (props) => {
-  const renderedList = props.videos.map((video) => {
-    return <VideoItem video={video} />;
+const VideoList = ({ videos, onVideoSelect }) => {
+  const renderedList = videos.map((video) => {
+    return (
+      <VideoItem
+        key={video.id.videoId}
+        onVideoSelect={onVideoSelect}
+        video={video}
+      />
+    );
   });
 
-  return <div>{renderedList}</div>;
+  return <div className='ui relaxed divided list'>{renderedList}</div>;
 };
 
 export default VideoList;
