791f41dde53c1b3cc1da762b145a24456398e597
joshembling <65712975+joshembling@users.noreply.github.com>
2020-11-24 14:28:55 +0000

    Updated app

--- src/components/App.js
+++ src/components/App.js
@@ -53,7 +53,7 @@ class App extends React.Component {
     return (
       <div className='ui container wrap'>
         <div className='icon'>
-          <a href='http://youtube.com' target='_blank' rel='youtube'>
+          <a href='http://youtube.com' target='_blank' rel='noreferrer'>
             <i class='fab fa-youtube fa-6x'></i>
           </a>
         </div>
